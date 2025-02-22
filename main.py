d87396d5d77a008476a86508672974c5f508e4d8
MIKHAIL-AYTHER <gamexforgot@gmail.com>
2025-02-22 15:33:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from pygame import *
 
 # клас-батько для спрайтів
@@ -44,6 +43,21 @@ class Enemy(GameSprite):
         else:
             self.rect.x += self.speed
 
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self.height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 win_width = 700
 win_height = 500
@@ -58,6 +72,15 @@ player = Player('hero.png', 5, win_height - 80, 4)
 monster = Enemy('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
+w1 = Wall(154, 205, 50, 100, 20, 450, 10)
+w2 = Wall(154, 205, 50, 100, 480, 350, 10)
+w3 = Wall(154, 205, 50, 100, 20, 10, 380)
+
+font.init()
+font = font.Font(None,70)
+win = font.render('YOU WIN!', True, (255, 215, 0))
+lose = font.render('YOU LOSEEER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', True, (180, 0, 0))
+
 game = True
 clock = time.Clock()
 FPS = 30
@@ -65,8 +88,11 @@ finish = False
 
 # музика
 mixer.init()
-mixer.music.load('jungles.ogg')
+mixer.music.load('Смешарики-От-винта_.ogg')
 mixer.music.play()
+
+money = mixer.Sound('money.ogg')
+kick = mixer.Sound('kick.ogg')
 
 while game:
     for e in event.get():
@@ -81,6 +107,20 @@ while game:
     window.blit(background, (0, 0))
     player.reset()
     monster.reset()
+    final.reset()
+
+    w1.draw_wall()
+    w2.draw_wall()
+    w3.draw_wall()
+
+    if (sprite.collide_rect(player, monster)
+        or sprite.collide_rect(player, w1)
+        or sprite.collide_rect(player, w2)
+        or sprite.collide_rect(player, w3)):
+        window.blit(lose, (200, 200))
+        if not finish:
+            kick.play()
+            finish = True
 
     display.update()
     clock.tick(FPS)
